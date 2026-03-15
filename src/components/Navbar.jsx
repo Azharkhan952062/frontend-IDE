a4162cb7c50e50453ec851fc154ebf028fa7ec79
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-03-16 04:06:40 +0530

    responsive update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,14 +25,14 @@ const Navbar = () => {
 
   return (
     <div className="sticky top-0 z-50 backdrop-blur-xl bg-[#020617]/95 border-b border-white/10">
-      <div className="max-w-[1600px] mx-auto px-14 py-6 flex items-center justify-between">
+      <div className="max-w-[1600px] mx-auto px-6 md:px-14 py-4 md:py-6 flex items-center justify-between">
 
         {/* LEFT SIDE : LOGO */}
-        <div className="flex items-center gap-5 cursor-pointer" onClick={() => navigate("/")}>
+        <div className="hidden md:flex items-center gap-5 cursor-pointer" onClick={() => navigate("/")}>
           <div className="w-14 h-14 rounded-2xl bg-gradient-to-br from-green-400 to-blue-500 flex items-center justify-center font-bold text-black text-2xl">
             K
           </div>
-          <h1 className="text-4xl font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent">
+          <h1 className="text-2xl md:text-4xl font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent">
             KodeBase
           </h1>
         </div>
